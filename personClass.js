d2a37355e8ad8befb39647a79a032e2cad4ddfa1
Balajiprasath T <82133700+Balajiprasath107@users.noreply.github.com>
2024-06-02 20:21:10 +0530

    Create personClass.js

---
+++ personClass.js
@@ -0,0 +1,13 @@
+class person{
+  constructor( firstName,lastName,age, mobileNo,  emailId)
+  {
+      this.firstName = firstName
+      this.lastName = lastName
+      this.age = age
+      this.mobileNo = mobileNo
+      this.emailId = emailId
+}
+
+}
+const person1 = new person("rahul","bobo",25,584684,"bobo@jmail")
+console.log(person1)
